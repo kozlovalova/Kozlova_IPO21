bddf1a0c2d4efec9be4e6f41e854fe9fb23f48c6
kozlovalova <kozlova114930@gmail.com>
2025-12-03 00:03:13 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,380 @@
+﻿namespace Алгоритмы
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBoxR = new System.Windows.Forms.TextBox();
+            this.textBoxY = new System.Windows.Forms.TextBox();
+            this.textBoxZ = new System.Windows.Forms.TextBox();
+            this.textBoxX = new System.Windows.Forms.TextBox();
+            this.labelZ = new System.Windows.Forms.Label();
+            this.labelY = new System.Windows.Forms.Label();
+            this.labelX = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.richTextBox = new System.Windows.Forms.RichTextBox();
+            this.checkBred = new System.Windows.Forms.CheckBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rbExt = new System.Windows.Forms.RadioButton();
+            this.rbSqr = new System.Windows.Forms.RadioButton();
+            this.rbCos = new System.Windows.Forms.RadioButton();
+            this.button2 = new System.Windows.Forms.Button();
+            this.tbY = new System.Windows.Forms.TextBox();
+            this.tbX = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.tabPage2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(226, 29);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1055, 622);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.textBoxR);
+            this.tabPage1.Controls.Add(this.textBoxY);
+            this.tabPage1.Controls.Add(this.textBoxZ);
+            this.tabPage1.Controls.Add(this.textBoxX);
+            this.tabPage1.Controls.Add(this.labelZ);
+            this.tabPage1.Controls.Add(this.labelY);
+            this.tabPage1.Controls.Add(this.labelX);
+            this.tabPage1.Controls.Add(this.pictureBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1047, 593);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Линейный";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Yi Baiti", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(466, 248);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 38);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Вычислить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBoxR
+            // 
+            this.textBoxR.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.textBoxR.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxR.Location = new System.Drawing.Point(19, 310);
+            this.textBoxR.Multiline = true;
+            this.textBoxR.Name = "textBoxR";
+            this.textBoxR.Size = new System.Drawing.Size(699, 277);
+            this.textBoxR.TabIndex = 7;
+            this.textBoxR.Text = "Тут будет ответ :)";
+            this.textBoxR.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // textBoxY
+            // 
+            this.textBoxY.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.textBoxY.Font = new System.Drawing.Font("Microsoft Yi Baiti", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxY.Location = new System.Drawing.Point(273, 196);
+            this.textBoxY.Name = "textBoxY";
+            this.textBoxY.Size = new System.Drawing.Size(122, 28);
+            this.textBoxY.TabIndex = 6;
+            // 
+            // textBoxZ
+            // 
+            this.textBoxZ.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.textBoxZ.Font = new System.Drawing.Font("Microsoft Yi Baiti", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxZ.Location = new System.Drawing.Point(273, 248);
+            this.textBoxZ.Name = "textBoxZ";
+            this.textBoxZ.Size = new System.Drawing.Size(122, 28);
+            this.textBoxZ.TabIndex = 5;
+            // 
+            // textBoxX
+            // 
+            this.textBoxX.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.textBoxX.Font = new System.Drawing.Font("Microsoft Yi Baiti", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxX.Location = new System.Drawing.Point(273, 142);
+            this.textBoxX.Name = "textBoxX";
+            this.textBoxX.Size = new System.Drawing.Size(122, 28);
+            this.textBoxX.TabIndex = 4;
+            // 
+            // labelZ
+            // 
+            this.labelZ.AutoSize = true;
+            this.labelZ.Font = new System.Drawing.Font("Microsoft Yi Baiti", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelZ.Location = new System.Drawing.Point(51, 248);
+            this.labelZ.Name = "labelZ";
+            this.labelZ.Size = new System.Drawing.Size(182, 20);
+            this.labelZ.TabIndex = 3;
+            this.labelZ.Text = "Введите значение Z:";
+            // 
+            // labelY
+            // 
+            this.labelY.AutoSize = true;
+            this.labelY.Font = new System.Drawing.Font("Microsoft Yi Baiti", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelY.Location = new System.Drawing.Point(50, 196);
+            this.labelY.Name = "labelY";
+            this.labelY.Size = new System.Drawing.Size(183, 20);
+            this.labelY.TabIndex = 2;
+            this.labelY.Text = "Введите значение Y:";
+            // 
+            // labelX
+            // 
+            this.labelX.AutoSize = true;
+            this.labelX.Font = new System.Drawing.Font("Microsoft Yi Baiti", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelX.Location = new System.Drawing.Point(50, 148);
+            this.labelX.Name = "labelX";
+            this.labelX.Size = new System.Drawing.Size(182, 20);
+            this.labelX.TabIndex = 1;
+            this.labelX.Text = "Введите значение X:";
+            this.labelX.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(19, 28);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(851, 90);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.richTextBox);
+            this.tabPage2.Controls.Add(this.checkBred);
+            this.tabPage2.Controls.Add(this.groupBox1);
+            this.tabPage2.Controls.Add(this.button2);
+            this.tabPage2.Controls.Add(this.tbY);
+            this.tabPage2.Controls.Add(this.tbX);
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Controls.Add(this.label3);
+            this.tabPage2.Controls.Add(this.pictureBox2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1047, 593);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Разветвляющийся";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox
+            // 
+            this.richTextBox.BackColor = System.Drawing.Color.MistyRose;
+            this.richTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBox.Location = new System.Drawing.Point(31, 345);
+            this.richTextBox.Name = "richTextBox";
+            this.richTextBox.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.richTextBox.Size = new System.Drawing.Size(496, 215);
+            this.richTextBox.TabIndex = 21;
+            this.richTextBox.Text = "Тут будет ответ :)";
+            this.richTextBox.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged_1);
+            // 
+            // checkBred
+            // 
+            this.checkBred.AutoSize = true;
+            this.checkBred.Font = new System.Drawing.Font("Microsoft Yi Baiti", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBred.ForeColor = System.Drawing.Color.Red;
+            this.checkBred.Location = new System.Drawing.Point(562, 370);
+            this.checkBred.Name = "checkBred";
+            this.checkBred.Size = new System.Drawing.Size(224, 24);
+            this.checkBred.TabIndex = 19;
+            this.checkBred.Text = "Ответ красным цветом";
+            this.checkBred.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rbExt);
+            this.groupBox1.Controls.Add(this.rbSqr);
+            this.groupBox1.Controls.Add(this.rbCos);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Yi Baiti", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(562, 197);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(248, 157);
+            this.groupBox1.TabIndex = 18;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Выберите функцию:";
+            // 
+            // rbExt
+            // 
+            this.rbExt.AutoSize = true;
+            this.rbExt.Location = new System.Drawing.Point(21, 113);
+            this.rbExt.Name = "rbExt";
+            this.rbExt.Size = new System.Drawing.Size(80, 22);
+            this.rbExt.TabIndex = 2;
+            this.rbExt.TabStop = true;
+            this.rbExt.Text = "Ext(x)";
+            this.rbExt.UseVisualStyleBackColor = true;
+            // 
+            // rbSqr
+            // 
+            this.rbSqr.AutoSize = true;
+            this.rbSqr.Location = new System.Drawing.Point(21, 72);
+            this.rbSqr.Name = "rbSqr";
+            this.rbSqr.Size = new System.Drawing.Size(80, 22);
+            this.rbSqr.TabIndex = 1;
+            this.rbSqr.TabStop = true;
+            this.rbSqr.Text = "Sqr(x)";
+            this.rbSqr.UseVisualStyleBackColor = true;
+            // 
+            // rbCos
+            // 
+            this.rbCos.AutoSize = true;
+            this.rbCos.Location = new System.Drawing.Point(17, 34);
+            this.rbCos.Name = "rbCos";
+            this.rbCos.Size = new System.Drawing.Size(84, 22);
+            this.rbCos.TabIndex = 0;
+            this.rbCos.TabStop = true;
+            this.rbCos.Text = "Cos(x)";
+            this.rbCos.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Yi Baiti", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(257, 301);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(150, 38);
+            this.button2.TabIndex = 17;
+            this.button2.Text = "Вычислить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // tbY
+            // 
+            this.tbY.BackColor = System.Drawing.Color.MistyRose;
+            this.tbY.Font = new System.Drawing.Font("Microsoft Yi Baiti", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbY.Location = new System.Drawing.Point(270, 261);
+            this.tbY.Name = "tbY";
+            this.tbY.Size = new System.Drawing.Size(122, 28);
+            this.tbY.TabIndex = 15;
+            // 
+            // tbX
+            // 
+            this.tbX.BackColor = System.Drawing.Color.MistyRose;
+            this.tbX.Font = new System.Drawing.Font("Microsoft Yi Baiti", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbX.Location = new System.Drawing.Point(270, 207);
+            this.tbX.Name = "tbX";
+            this.tbX.Size = new System.Drawing.Size(122, 28);
+            this.tbX.TabIndex = 13;
+            this.tbX.TextChanged += new System.EventHandler(this.tbX_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Yi Baiti", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(47, 261);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(183, 20);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Введите значение Y:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Yi Baiti", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(47, 213);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(182, 20);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Введите значение X:";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(16, 6);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(730, 185);
+            this.pictureBox2.TabIndex = 9;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1371, 684);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label labelX;
+        private System.Windows.Forms.TextBox textBoxY;
+        private System.Windows.Forms.TextBox textBoxZ;
+        private System.Windows.Forms.TextBox textBoxX;
+        private System.Windows.Forms.Label labelZ;
+        private System.Windows.Forms.Label labelY;
+        private System.Windows.Forms.TextBox textBoxR;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox tbY;
+        private System.Windows.Forms.TextBox tbX;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.RadioButton rbExt;
+        private System.Windows.Forms.RadioButton rbSqr;
+        private System.Windows.Forms.RadioButton rbCos;
+        private System.Windows.Forms.CheckBox checkBred;
+        private System.Windows.Forms.RichTextBox richTextBox;
+    }
+}
+
